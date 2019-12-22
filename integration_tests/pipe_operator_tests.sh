d72ae96f228dbb64a2f623be0b5e42d35c9c8be8
Marc Jimenez <marcjimenez999@gmail.com>
2019-12-22 14:31:44 -0800

    Recommit

--- integration_tests/pipe_operator_tests.sh
+++ integration_tests/pipe_operator_tests.sh
@@ -10,7 +10,7 @@ echo ABC | tr A-Z a-z
 cat < integration_tests/inputfile.txt | tr A-Z a-z | tr a-z A-Z
 cat < integration_tests/inputfile.txt | sort
 cat < integration_tests/inputfile.txt | tr a-z A-Z | sort > integration_tests/outputfile.txt
-cat < integration_tests/inputfile2.txt | tr A-Z a-z | tee integration_tests/outputfile.txt | tr a-z A-Z >> integration_tests/outputfile.txt
-exit 
+exit
+
 EOF
 
